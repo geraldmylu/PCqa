63c13cd8becff3f2a2305d341fbdc068f3a7d522
geraldmylu <geraldmylu@gmail.com>
2019-08-29 15:52:35 +0800

    Login to branch 85

--- Scripts/Login/Script1567062891812.groovy
+++ Scripts/Login/Script1567062891812.groovy
@@ -12,4 +12,27 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://bms-ppd.pcdsi.ph/login')
+
+WebUI.click(findTestObject('Object Repository/Page_BMS/button_Login'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Sign in to your account/input_Sign in_loginfmt'), 'bdangeles@pcdsi.net')
+
+WebUI.click(findTestObject('Object Repository/Page_Sign in to your account/input_Sign in_idSIButton9'))
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Sign in to your account/input_Enter password_passwd'), 'GY2JYY+u2OfRbbxKoeevjQ==')
+
+WebUI.click(findTestObject('Object Repository/Page_Sign in to your account/input_Forgot my password_idSIButton9'))
+
+WebUI.click(findTestObject('Object Repository/Page_Sign in to your account/input_concat(Don  t show this again)_idBtn_Back'))
+
+WebUI.click(findTestObject('Object Repository/Page_BMS/div_Branch'))
+
+WebUI.click(findTestObject('Object Repository/Page_BMS/span_0085 - Novaliches Quezon City'))
+
+WebUI.click(findTestObject('Object Repository/Page_BMS/button_Proceed'))
 
